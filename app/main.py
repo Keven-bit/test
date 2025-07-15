71ebe981f846fdaab45d2a1398f83ad8d7e3c9b2
WangQirui <wqr23@mails.tsinghua.edu.cn>
2025-07-15 21:12:05 +0800

    feat(main): create initial admin user when start up

--- app/main.py
+++ app/main.py
@@ -1,16 +1,42 @@
 from fastapi import FastAPI
 from .api import problems, submissions, languages, users, auth
 from .core import errors
+from .db.schemas import *
 from fastapi.exceptions import HTTPException, RequestValidationError
 from contextlib import asynccontextmanager
-from .db.database import create_db_and_tables
+from .db.database import create_db_and_tables, get_db_async_session, engine
+from sqlmodel import select
 
 
-# Async table generation when start up
+# Initial admin user password
+INITIAL_ADMIN_PASSWORD = "admintestpassword"
+
+
+# Start up logics
 @asynccontextmanager
 async def lifespan(app: FastAPI):
+    # Async table generation
     await create_db_and_tables()
+    
+    # Creat initial admin user
+    async for session in get_db_async_session():
+        statement = select(UserItem).where(UserItem.username == "admin")
+        admin_user = await session.execute(statement).first()
+        if not admin_user:
+            admin_create = UserCreate(
+                username="admin",
+                password=INITIAL_ADMIN_PASSWORD
+            )
+            new_admin = UserItem.create_with_hashed_password(admin_create)
+            session.add(new_admin)
+            await session.commit()
+            await session.refresh(new_admin)
+            print(f"Initial admin user 'admin' created. admin id: '{new_admin.id}'")
+        else:
+            print(f"Initial admin user 'admin' already exists. admin id: '{admin_user.id}'")
+            
     yield
+    await engine.dispose()
 
 app = FastAPI(
     title="Simple OJ System - Student Template",
@@ -23,6 +49,7 @@ app = FastAPI(
 app.exception_handler(HTTPException)(errors.custom_http_exception_handler)
 app.exception_handler(RequestValidationError)(errors.custom_validation_exception_handler)
 
+# Register routers
 app.include_router(problems.problems_router)
 app.include_router(submissions.submissions_router)
 app.include_router(languages.languages_router)
